1621cfcc8cd09e2feff8f005e4838143a6bc3dc6
Moulika-Isn <Moulika-Isn@missionrnd.com>
2016-03-30 19:57:00 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -11,6 +11,7 @@ ERROR CASES: Return NULL for error cases.
 NOTES:
 */
 
+#include <malloc.h>
 #include <stdio.h>
 
 struct node {
@@ -18,6 +19,85 @@ struct node {
 	struct node *next;
 };
 
+int length(struct node* head)
+{
+	int i = 0;
+	while (head != NULL)
+	{
+		i++;
+		head = head->next;
+	}
+	return i;
+}
+void insert(node **head, int ele)
+{
+	node *p, *temp;
+	temp = (node*)malloc(sizeof(node*));
+	temp->num = ele;
+	temp->next = NULL;
+	if (head == NULL)
+		*head = temp;
+	else
+	{
+		p = *head;
+		p->next = temp;
+	}
+}
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	if ((head1 == NULL) && (head2 == NULL))
+		return NULL;
+	if ((head1 == NULL) && (head2 != NULL))
+		return head2;
+	else if ((head1 != NULL) && (head2 == NULL))
+		return head1;
+	struct node *head, *p, *q, *r;
+	int i, j = 0, k = 0, m, n;
+	head = (node*)malloc(sizeof(node*));
+	p = head1;
+	q = head2;
+	r = head;
+	m = length(p);
+	n = length(q);
+	for (i = 0; i < m + n;)
+	{
+		if ((j < m) && (k < n))
+		{
+			if (p->num <= q->num)
+			{
+				insert(&r, p->num);
+				r = r->next;
+				p = p->next;
+				i++; j++;
+			}
+			else
+			{
+				insert(&r, q->num);
+				r = r->next;
+				q = q->next;
+				i++; k++;
+			}
+		}
+		else if (j == m)
+		{
+			for (; i < m + n;)
+			{
+				insert(&r, q->num);
+				r = r->next;
+				i++;
+				q = q->next;
+			}
+		}
+		else
+		{
+			for (; i < m + n;)
+			{
+				insert(&r, p->num);
+				r = r->next;
+				p = p->next;
+				i++;
+			}
+		}
+	}
+	head = head->next;
+	return head;
 }
